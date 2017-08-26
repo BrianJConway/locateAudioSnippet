8aa78e15ff0c658bb15f003b83e50b94d0f8c82d
BrianJConway <brianc@nevada.unr.edu>
2017-08-26 02:54:43 -0700

    fixed positive example labeling to handle multiple episodes

--- imgToNums.py
+++ imgToNums.py
@@ -11,28 +11,42 @@ import re
 
 resultsFile = open('y.txt', 'w')
 
-# Create sorted list of image files
-imageFiles = { }
-maxChunk = -1
+episodeNums = [ ]
 
 for imageFile in os.listdir('chunkImages'):
-    # Get chunk number from file name
-    match = re.search(r'_(\d+).', imageFile)
-    chunkNumber = int(match.group(1))
+    match = re.search(r'mbmbam(\d+)_(\d+)(_POS)?.png', imageFile);
+    if int(match.group(1)) not in episodeNums:
+        episodeNums.append(int(match.group(1)))
 
-    # Check if new max chunk value
-    if maxChunk < chunkNumber:
-        maxChunk = chunkNumber
+print(episodeNums)
+episodeNums.sort()
 
-    # Save chunk and filename in dictionary
-    imageFiles[int(chunkNumber)] = imageFile
+for episode in episodeNums:
 
-for chunkNum in range(maxChunk + 1):
-    print('Processing ' + imageFiles[chunkNum] + '...')
-    if imageFiles[chunkNum].endswith('POS.png'):
-        resultsFile.write('1 \n')
-    else:
-        resultsFile.write('0 \n')
-    shutil.copy('chunkImages/' + imageFiles[chunkNum], 'trainingSetImages')
+    imageFiles = { }
+    maxChunk = -1;
+
+    for imageFile in os.listdir('chunkImages'):
+        # Get chunk number from file name
+        match = re.search(str(episode) + r'_(\d+).', imageFile)
+
+        if match:
+            chunkNumber = int(match.group(1))
+
+            # Check if new max chunk value
+            if maxChunk < chunkNumber:
+                maxChunk = chunkNumber
+
+            # Save chunk and filename in dictionary
+            imageFiles[int(chunkNumber)] = imageFile
+
+    for chunkNum in range(maxChunk + 1):
+        print('Processing ' + imageFiles[chunkNum] + '...')
+        if imageFiles[chunkNum].endswith('POS.png'):
+            resultsFile.write('1 \n')
+            shutil.copy('chunkImages/' + imageFiles[chunkNum], 'positiveExamples')
+        else:
+            resultsFile.write('0 \n')
+        shutil.copy('chunkImages/' + imageFiles[chunkNum], 'trainingSetImages')
 
 resultsFile.close()
