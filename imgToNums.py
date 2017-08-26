4c3fd22c191884ab5e4ab286db7f0aca6a816c4c
BrianJConway <brianc@nevada.unr.edu>
2017-08-25 23:43:36 -0700

    fixed bug where filenames would be processed alphabetically rather than numerically

--- imgToNums.py
+++ imgToNums.py
@@ -3,21 +3,35 @@ import os
 import shutil
 import pprint
 import send2trash
+import re
 
-resultsFile = open('Y.txt', 'a')
+# Done like this because I need image files processed
+# in correct numerical order, and sorting the filenames
+# alphabetically sorts them wrong
+
+resultsFile = open('y.txt', 'a')
 
 # Create sorted list of image files
-imgFiles = os.listdir('chunkImages')
-imgFiles.sort()
+imageFiles = { }
+maxChunk = -1
 
-for currentImg in imgFiles:
-    print('Processing ' + currentImg + ' ... ', end='', flush=True)
-    # Note whether or not this is a positive example
-    if currentImg.endswith('POS.png'):
+for imageFile in os.listdir('chunkImages'):
+    # Get chunk number from file name
+    match = re.search(r'_(\d+).', imageFile)
+    chunkNumber = int(match.group(1))
+
+    # Check if new max chunk value
+    if maxChunk < chunkNumber:
+        maxChunk = chunkNumber
+
+    # Save chunk and filename in dictionary
+    imageFiles[int(chunkNumber)] = imageFile
+
+for chunkNum in range(maxChunk + 1):
+    print('Processing ' + imageFiles[chunkNum] + '...')
+    if imageFiles[chunkNumber].endswith('POS.png'):
         resultsFile.write('1 \n')
     else:
         resultsFile.write('0 \n')
-
-    print()
 
 resultsFile.close()
