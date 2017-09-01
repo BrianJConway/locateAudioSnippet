cda034618d0acb74ece5fc94c4aaa10f00d545cd
BrianJConway <brianc@nevada.unr.edu>
2017-08-31 21:50:51 -0700

    removed duplicated code

--- locateAudio.py
+++ locateAudio.py
@@ -4,6 +4,9 @@ import shutil, natsort
 from pydub import AudioSegment
 from matplotlib import pyplot as plt
 import spectrogram
+import makeDataset
+
+path = '/media/linux/Flash/mbmbam/wav/'
 
 # Maps values from 0 to 1
 def sigmoid(x):
@@ -31,90 +34,13 @@ def applyHypothesis(X, theta, threshold):
     # Apply hypothesis function (parameterized by theta) to X
     return sigmoid(np.dot(X, theta)) >= threshold
 
-def cutEndsOff(podcastFile):
-    twentyOneMins27Secs = (21 * 60  + 27) * 1000
-    seventeenMins = 17 * 60 * 1000
-    podcast = AudioSegment.from_wav(podcastFile)
-    podcast = podcast[twentyOneMins27Secs:len(podcast) - seventeenMins]
-    podcast.export("current.wav", format="wav")
-
-    return podcast
-
-# Generate flattened 50x100 matrix of current chunk
-def processChunk(chunkData):
-    chunkHeight = int(chunkData.shape[0] / 50)
-    chunkWidth = int(chunkData.shape[1] / 100)
-
-    avgVals = np.zeros((50,100))
-
-    for row in range(len(avgVals)):
-        for col in range(len(avgVals[row])):
-            
-            rowStart = row * chunkHeight
-            rowEnd = rowStart + chunkHeight
-            colStart = col * chunkWidth
-            colEnd = colStart + chunkWidth
-
-            currentChunk = chunkData[rowStart:rowEnd,colStart:colEnd]
-            avgVals[row][col] = np.mean(currentChunk)
-
-    return avgVals.flatten()
-
-def makeImage(chunkData, fileName, colormap="jet"):
-    plt.figure(figsize=(2, 1),dpi=60)
-    plt.imshow(chunkData, origin="lower", aspect="auto", cmap=colormap, interpolation="none")
-
-    plt.xticks([])
-    plt.yticks([])
-
-    plt.savefig( 'chunkImages/' + fileName + '.png', dpi='figure', bbox_inches="tight", pad_inches=0)
-            
-    plt.clf()
-    plt.close()
-
-def splitIntoChunks(podcastData, podcastAudio, fileName, genImages):
-    strideSeconds = 6
-    chunkDurationSeconds = 13
-    num_chunks = int(podcastAudio.duration_seconds / chunkDurationSeconds)
-
-    rowsPerChunk = podcastData.shape[0]
-    columnsPerSecond =  podcastData.shape[1] / podcastAudio.duration_seconds 
-    columnsPerChunk = int(chunkDurationSeconds * columnsPerSecond)
-
-    trainingExamples = open('X.txt', 'w')
-
-    print('File: ' + fileName)
-    print('Duration: ' + str(int(podcastAudio.duration_seconds)) +' seconds.')
-
-    lastChunk = int(podcastAudio.duration_seconds / strideSeconds)
-
-    for chunk in range(lastChunk):
-        print('Processing Chunk ' + str(chunk) + '/' + str(lastChunk - 1))
-        rowStart = 0
-        rowEnd = 513
-        colStart = chunk * (strideSeconds * columnsPerSecond)
-        colEnd = colStart + columnsPerChunk
-
-        chunkData = podcastData[rowStart:rowEnd,colStart:colEnd]
-
-        if genImages:
-            makeImage(chunkData, fileName[:-4] + '_' + str(chunk))
-
-        singleRow = processChunk(chunkData)
-
-        for currentVal in singleRow:
-            trainingExamples.write(str(currentVal) + ' ')
-        trainingExamples.write('\n')
-
-    trainingExamples.close()
-
 def locate(path, fileName, genImages):
-    middleSegment = cutEndsOff(path + fileName)
+    middleSegment = makeDataset.cutEndsOff(path + fileName)
 
     # Short time Fourier Transform of podcast file
     audioData = spectrogram.plotstft('current.wav')
 
-    splitIntoChunks(audioData, middleSegment, fileName, False)
+    makeDataset.splitIntoChunks(audioData, middleSegment, fileName, False)
 
     # Load X and theta matrices from file
     X = np.loadtxt('X.txt')
