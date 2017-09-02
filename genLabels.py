e5d86c3123e7cd35eb80832e1543aa4c07f003f6
BrianJConway <brianc@nevada.unr.edu>
2017-09-02 15:47:51 -0700

    more documentation, made file i/o multiplatform

--- genLabels.py
+++ genLabels.py
@@ -1,13 +1,9 @@
-import pprint
-import re
-import natsort
-import send2trash
-import os
+import pprint, re, natsort, send2trash, os
 from pydub import AudioSegment
 
-
-path = '/media/linux/Flash/mbmbam/'
-
+# Creates a dictionary of episode numbers as keys and lists of
+# chunk numbers for that episode as values
+# File format for each line: "EPISODENUM: chunk1 chunk2 ... chunkN"
 def loadChunksFile():
     # Read chunk info for each episode into dictionary
     chunksFile = open('episodeChunks.txt')
@@ -17,10 +13,14 @@ def loadChunksFile():
 
     posExamples = {}
     for currentLine in lines:
+        # Get list of each word in the line
         values = currentLine.split()
+
+        # Get the episode number, removing the colon at the end
         episode = int(values[0][:-1])
         posExamples.setdefault(episode, [])
 
+        # Get each chunk numberk, add to list of chunks for that episode
         for chunkIndex in range(1, len(values)):
             posExamples[episode].append(int(values[chunkIndex]))
     chunksFile.close()
@@ -29,8 +29,7 @@ def loadChunksFile():
 
 
 def labelOneEpisode(episodeAudio, episodeNum, posChunks):
-    labelsFile = open('/home/linux/Desktop/AdFinder/dataLabels/' +
-                      str(episodeNum) + 'y.txt', 'w')
+    labelsFile = open(os.path.join('dataLabels', str(episodeNum) + 'y.txt'), 'w')
     podcastDuration = episodeAudio.duration_seconds
     numChunks = podcastDuration / 6
 
@@ -47,16 +46,16 @@ def combileFiles():
     sortedFiles = natsort.natsorted(labelsFiles)
 
     # Combine all label files to create one giant file of labels
-    allLabels = open('/home/linux/Desktop/AdFinder/dataLabels/y.txt', 'w')
+    allLabels = open(os.path.join('dataLabels', 'y.txt'), 'w')
     for currentFile in sortedFiles:
-        labelsFile = open('/home/linux/Desktop/AdFinder/dataLabels/' + currentFile)
+        labelsFile = open(os.path.join('dataLabels', currentFile), 'r')
         episodeLabels = labelsFile.read()
         allLabels.write(episodeLabels)
         labelsFile.close()
     allLabels.close()
 
 
-def fromTxtFile():
+def fromTxtFile(path):
     posExamples = loadChunksFile()
 
     if 'dataLabels' not in os.listdir():
@@ -109,74 +108,3 @@ def fromImages():
                 resultsFile.write(match.group(2) + ' ')
         resultsFile.write('\n')
     chunksFile.close()
-
-"""
-timings = {}
-
-for i in range(len(line)):
-    # remove newlines from end of each string
-    line[i] = line[i][:-1]
-
-    # Find episode number, minutes, and seconds
-    match = re.search(r'mbmbam(\d+).wav: (\d+):(\d+)', line[i]);
-    episode = int(match.group(1))
-    minutes = int(match.group(2))
-    seconds = int(match.group(3))
-
-    timings.setdefault(episode, [])
-    totalSeconds = minutes * 60 + seconds
-    currentExample = {'minutes': minutes, 'seconds': seconds,
-                      'totalSeconds': totalSeconds, 'chunk': (totalSeconds - 1287) / 6}
-    timings[episode].append(currentExample)
-
-times = open('oldTimings', 'w')
-
-for episode in timings.keys():
-    times.write(str(episode) + ': ')
-    for example in timings[episode]:
-        times.write(str(example['chunk'])[:-2] + ' ')
-    times.write('\n')
-times.close()
-
-episodes = timings.keys()
-
-path = '/media/linux/Flash/mbmbam/'
-
-thirteenSeconds = 13 * 1000
-
-for episode in timings.keys():
-    delete = []
-    if len(timings[episode]) == 1:
-        episodeAudio = AudioSegment.from_wav(
-            path + 'mbmbam' + str(episode) + '.wav')
-        for example in timings[episode]:
-            chunkStart = example['totalSeconds'] * 1000
-            chunkEnd = chunkStart + thirteenSeconds
-
-            chunk = episodeAudio[chunkStart:chunkEnd]
-            fileName = str(episode) + '_' + str(example['chunk']) + '.wav'
-            chunk.export('located/' + fileName, format="wav")
-
-        for example in timings[episode]:
-            fileName = str(episode) + '_' + str(example['chunk']) + '.wav'
-            print(fileName + ': is this chunk valid? (Y/N)')
-            valid = input()
-
-            if valid.upper() == 'N':
-                print('Deleting ' + fileName + '...')
-                send2trash.send2trash('located/' + fileName)
-                delete.append(example)
-
-    for item in delete:
-        timings[episode].remove(item)
-
-times = open('newTimings.txt', 'a')
-
-for episode in timings.keys():
-    times.write(str(episode) + ': ')
-    for example in timings[episode]:
-        times.write(str(example['chunk'])[:-2] + ' ')
-    times.write('\n')
-times.close()
-
-"""
